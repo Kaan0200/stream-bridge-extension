e254518920efd46658da8c50c1013c3593b3f46d
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-08-28 11:51:05 -0400

    added find and insert into content page

--- content.tsx
+++ content.tsx
@@ -1,12 +1,21 @@
 import type { PlasmoCSConfig } from "plasmo";
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export const config: PlasmoCSConfig = {
     matches: ['https://www.beatport.com/*'],
     world: "MAIN",
 }
 
-const LinkButton = () => {
+const LinkButton: () => JSX.Element = () => {
     return <button>Link To</button>
+}
+
+console.log("Attempting Match...");
+let target = document.querySelector('[data-testid="play-button"]');
+if (target) {
+    console.log('found');
+    console.log(target);
+    target.parentElement.insertAdjacentHTML('afterend', renderToStaticMarkup(LinkButton()));
 }
 
 export default LinkButton;
