0b64bf6470d6d82d27a4b705cdbd8474cda77349
Alan Kaan Atesoglu <kaan@jagenterprisesllc.com>
2025-08-28 14:59:14 -0400

    added background script, started Tidal API

--- popup.tsx
+++ popup.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import { finalizeLogin, init, initializeLogin } from "@tidal-music/auth/dist"
 
 function IndexPopup() {
+
+
   const [data, setData] = useState("")
 
   return (
@@ -9,18 +12,39 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+        Stream Bridge
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <button
+        onClick={() => TidalLoginFlow()}
+        style={{
+          width: 120,
+          height: 48,
+          backgroundColor: "black",
+          color: "white",
+          borderStyle: "none",
+          borderRadius: 8,
+          cursor: "pointer",
+          
+        }}
+      >
+        Login to TIDAL
+      </button>
     </div>
   )
+}
+
+async function TidalLoginFlow() {
+  const ClientID: string = "wzkJ9EGRVZyio8l2";
+  console.log("firing login flow");
+  await init({clientId: ClientID, credentialsStorageKey: 'authorizationCode'})
+  
+  const loginUrl = await initializeLogin({
+   redirectUri: "https://login.tidal.com/"
+  });
+
+  //finalizeLogin()
+
+  window.open(loginUrl, '_self');
 }
 
 export default IndexPopup
